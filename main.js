02476ab96d8a51119152485de580677634667e6e
Yousuke Nakano <yousuke.computer@gmail.com>
2020-08-06 21:17:00 +0000

    おおまかな形を作成

---
+++ main.js
@@ -0,0 +1,51 @@
+(function() {
+  'use strict';
+
+  const keyborad = document.getElementById('keyboard');
+
+  
+  let whitekey_div = [];
+  let blackkey_div = [];
+  (function() {
+    for (let i = 0; i < 15; i++) {
+      whitekey_div[i] = document.createElement('div');
+      whitekey_div[i].className = 'whitekey';
+      whitekey_div[i].style.marginLeft = 1 + (93 * i) + 'px';
+      keyborad.appendChild(whitekey_div[i]);
+    }
+  })();
+/*
+  (function() {
+    for (let i = 0; i < 14; i++) {
+      blackkey_div[i] = document.createElement('div');
+      if (i % 7 == 2 || i % 7 == 6) {
+        blackkey_div[i].className = 'blackkey_none';
+      } else {
+        blackkey_div[i].className = 'blackkey';
+      }
+      keyborad.appendChild(blackkey_div[i]);
+    }
+  })()
+*/
+
+  (function() { 
+    let ith_marginleft;
+    for (let i = 0; i < 10; i++) {
+      blackkey_div[i] = document.createElement('div');
+      blackkey_div[i].className = 'blackkey';
+      if (i == 0) {
+        ith_marginleft = 70;
+      } else if (i % 5 == 2 || i % 5 == 0) {
+        ith_marginleft += 93 * 2;
+      } else {
+        ith_marginleft += 93;
+      }
+      blackkey_div[i].style.marginLeft = ith_marginleft + 'px';
+      keyborad.appendChild(blackkey_div[i]);
+    }
+  })();
+
+
+
+
+})();
