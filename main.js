167df1c8f1ffad6741be6cbf676f314904a683f0
Yousuke Nakano <yousuke.computer@gmail.com>
2020-08-07 03:55:36 +0000

    正しく動くかテストしてないのでわからないが，wavファイルを作成する関数を実装

--- main.js
+++ main.js
@@ -57,6 +57,87 @@
     }
   })();
 
+  var wav_header = {
+    RIFF: "RIFF",
+    file_size: 0,
+    WAVE: "WAVE",
+    FMT: "fmt ",
+    fmt_size: 16,
+    format: 1,
+    channel: 1,
+    sampling_fre: 44100,
+    bit_rate: 16,
+    byte_per_s: 0,
+    block_size: 0,
+    DATA: "data",
+    data_size: 0,
+    header_size: 4 + 4 + 4 + 4 + 4 + 2 + 2 + 4 + 4 + 2 + 2 + 4 + 4,
+    
+    set: function() {
+      this.byte_per_s = (this.channel * this.sampling_fre * (this.bit_rate / 8));
+      this.block_size = (this.channel * (this.bit_rate / 8));
+      console.log(this.block_size);
+    }
+  };
+  wav_header.set();
+
+  function int_to_string(n, byte) {
+    let string = "";
+    for (let i = 0; i < byte; i++) {
+      string += String.fromCharCode(n % 256);
+      n /= 256;
+    }
+    return string;
+  }
+
+  function create_sin_wave(wav_header, frequency, length) {
+    // bit_rateが16以外の場合は考えないものとする
+    if (wav_header.bit_rate != 16) {
+      console.error('bit_rate dose not 16.');
+      return;
+    }
+    let data_AB = new ArrayBuffer(wav_header.header_size + byte_per_s * length);
+    let data_ui8a = new Uint8Array(data_AB);
+    let wave = new Int16Array(wav_header.sampling_fre * length);
+    for (let i = 0; i < wave.length; i++) {
+      wave[i] = Math.floor(Math.sin(i * Math.pi * frequency / wav_header.sampling_fre));
+    }
+
+    wav_header.data_size = wav_header.byte_per_s * length;
+    wav_header.file_size = 4 + 4 + 4 + wav_header.fmt_size + wav_header.data_size + 4 + 4;
+
+    let header_string = "";
+    header_string += wav_header.RIFF;
+    header_string += int_to_string(wav_header.file_size, 4);
+    header_string += wav_header.FMT;
+    head_string += int_to_string(wav_header.fmt_size, 4);
+    head_string += int_to_string(wav_header.format, 2);
+    head_string += int_to_string(wav_header.channel, 2);
+    head_string += int_to_string(wav_header.sampling_fre, 4);
+    head_string += int_to_string(wav_header.byte_per_s, 4);
+    head_string += int_to_string(wav_header.block_size, 2);
+    head_string += int_to_string(wav_header.bit_rate, 2);
+    head_string += wav_header.DATA;
+    head_string += int_to_string(wav_header.data_size, 4);
+    
+    let data_string = "";
+    for (i = 0; i < wave.length; i++) {  
+      data_string += int_to_string(wave[i], 2);
+    }
+
+    let i;
+    for (i = 0; i < head_string.length; i++) {
+      data_ui8a[i] = head_string.charCodeAt(i);
+    }
+
+    for (i = 0; i < data_string; i++) {
+      data_ui8a[i + header_string.length] = data_string.charCodeAt(i);
+    }
+    
+    return data_AB;
+  }
+
+
   let A4_frequency = 440;
   let frequency_list = [];
   (function() {
